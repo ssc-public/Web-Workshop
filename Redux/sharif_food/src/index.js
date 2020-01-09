c87292e820f6c42c5c17c87b97f6a2aef3e3b5a0
amir <amirmahdidigbari@yahoo.com>
2020-01-09 16:39:11 +0330

    [Redux][Dev] add redux and redux-thunk. install react-router-dom but not use it yet

--- Redux/sharif_food/src/index.js
+++ Redux/sharif_food/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import App from './components/app/App';
+import * as serviceWorker from './serviceWorker';
+import store from './store';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
