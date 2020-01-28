d5d289ca52558aff71b7d718247db987be04813a
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2020-01-28 17:13:32 +0330

    [RestFramework][Dev] migrate from sqlite to postgres

--- Restful-API/shariffood/shariffood/settings.py
+++ Restful-API/shariffood/shariffood/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.users',
+    'apps.restaurants',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'shariffood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'test',
+        'PASSWORD': 'test',
+        'HOST': 'localhost',
+        'PORT': '',  
     }
 }
 
