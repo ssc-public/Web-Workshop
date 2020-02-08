d3dd79c2b64cbfb2ac04c5d9c151a4dc742efa2a
Rez <reza.amini.majd1379@gmail.com>
2020-02-08 22:59:32 +0330

    Rest Author add urls

---
+++ Restful-API/shariffood/apps/users/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from rest_framework import routers
+from rest_framework_simplejwt import views as jwt_views
+from . import views
+
+app_name = 'account'
+
+urlpatterns = [
+    path('login', jwt_views.TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('token/refresh', jwt_views.TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('password-update', views.ChangePassword.as_view(), name='password_update'),
+    path('password/reset', views.ForgotPasswordView.as_view()),
+
+]
