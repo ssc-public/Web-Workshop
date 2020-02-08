3f2cc77286cbb9d1ab9bd9f6f960d0e146e04687
Rez <reza.amini.majd1379@gmail.com>
2020-02-08 22:58:23 +0330

    Rest Author Change Password View

--- Restful-API/shariffood/apps/users/views.py
+++ Restful-API/shariffood/apps/users/views.py
@@ -1,13 +1,15 @@
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 import secrets
 from .models import ForgotPasswordToken
-from .serializers import ForgotPasswordSerializer
+from .serializers import ForgotPasswordSerializer, ChangePasswordSerializer
 from django.utils import timezone
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from rest_framework.generics import GenericAPIView, get_object_or_404
 from rest_framework.response import Response
 from .service import send_email
+from rest_framework import permissions, status
 
 
 class ForgotPasswordView(GenericAPIView):
@@ -29,3 +31,16 @@ class ForgotPasswordView(GenericAPIView):
         return Response({'detail': 'Successfully Sent Reset Password Email'}, status=200)
 
 
+class ChangePassword(GenericAPIView):
+    serializer_class = ChangePasswordSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            data = serializer.data
+        if not request.user.check_password(data['password']):
+            return Response(data={'errors': 'incorrect current password'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+        request.user.password = make_password(data['new_password'])
+        request.user.save()
+        return Response(data={'detail': 'password changed successfully'}, status=status.HTTP_200_OK)
