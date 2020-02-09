b6caf7584254f3a98eade02f07eda1386e1ee1a1
Mohammad Jamshidi <jamshidi.m799@gmail.com>
2020-02-09 09:31:13 +0330

    bug fix

---
+++ React/sharifood/src/component/common/Comment.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Comment extends Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+
+}
+
+export default Comment
