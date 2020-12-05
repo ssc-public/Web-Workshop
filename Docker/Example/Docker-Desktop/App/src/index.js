85aa7321667bd88e4de1a094fa72cbadf30d6499
saee <saee_saadat@outlook.com>
2020-12-05 11:40:55 +0330

    [Docker] [Dev] example complete with nginx

--- Docker/Example/Docker-Desktop/App/src/index.js
+++ Docker/Example/Docker-Desktop/App/src/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const app = express();
 app.use(express.static('public'));
+app.use(express.json());
 app.listen(3000, () => console.log('Node backend is Running!'));
 
-app.get("/node/time", (req, res) => {
+app.get("/getTime", (req, res) => {
     console.log("request received for time");
     var t = new Date().toISOString().match(/(\d{2}:){2}\d{2}/)[0];
-    res.send(`${t}`);
+    res.json(`${t}`).status(200).send();
 })
