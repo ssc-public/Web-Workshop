5376287ebfdf4fb25755b6557fa582719dce924c
mohammad <mohammad.mo@smartech.ir>
2024-07-06 20:19:51 +0330

    spring pagination

---
+++ Spring/Pagination/demo/src/test/java/edu/web/workshop/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.web.workshop.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
