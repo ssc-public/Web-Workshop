191a6dbf1c69ca83dca626b1479fe7fb96975237
rezavahidi <79265376+rezavahidi@users.noreply.github.com>
2025-02-03 22:44:12 +0000

    add spring boot testing files

---
+++ Spring/Spring Testing/unit-vs-int/src/main/java/test/dev/unit_vs_int/UnitVsIntApplication.java
@@ -0,0 +1,13 @@
+package test.dev.unit_vs_int;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitVsIntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitVsIntApplication.class, args);
+	}
+
+}
